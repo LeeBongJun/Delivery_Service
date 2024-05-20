91ae2d7b002b2019bd9010d0c143fc321fb588d3
bjl0615 <bjl0615@gmail.com>
2024-05-20 23:40:06 +0900

    API ERROR CODE

--- api/src/main/java/org/delivery/api/common/api/Api.java
+++ api/src/main/java/org/delivery/api/common/api/Api.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 @Data
 @NoArgsConstructor
@@ -22,4 +23,21 @@ public class Api<T> {
         return api;
     }
 
+    public static Api<Object>ERROR(Result result) {
+        var api = new Api<Object>();
+        api.result = result;
+        return api;
+    }
+
+    public static Api<Object>ERROR(ErrorCodeIfs errorCodeIfs , Throwable tx) {
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs , tx);
+        return api;
+    }
+
+    public static Api<Object>ERROR(ErrorCodeIfs errorCodeIfs , String description) {
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs , description);
+        return api;
+    }
 }
